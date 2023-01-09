98912f0b189d24e52f2ba7e07d1a7fa303e33249
samjaberi <samanjaberi.sj@gmail.com>
2023-01-09 12:50:32 -0500

    got monkeys on the page

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const artApp = {}
 artApp.apikey = 'rn7LSbf1'
+artApp.displayPieces = (pieces) => {
+    console.log(pieces)
+    pieces.forEach(artObject => {
+       const title = document.createElement('h2');
+        title.innerText = artObject.title;
+        const artist = document.createElement('p');
+        artist.innerText = artObject.principalOrFirstMaker;
+        const image = document.createElement('img');
+        image.src = artObject.webImage.url;
+        image.alt = artObject.title;
+        const artPiece = document.createElement('div');
+        artPiece.classList.add('piece');
+        artPiece.appendChild(title);
+        artPiece.appendChild(artist);
+        artPiece.appendChild(image);
+        document.querySelector('#artwork').appendChild(artPiece)
+        
+    });
+
+
+}
 artApp.getPieces = (url) => {
     url = new URL(url)
     url.search = new URLSearchParams({
@@ -13,6 +34,7 @@ artApp.getPieces = (url) => {
     })
     .then((jsonData) => {
         console.log(jsonData.artObjects)
+        artApp.displayPieces(jsonData.artObjects)
     })
 
 }
